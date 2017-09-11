3fb5db0261a86f7686f3120ceca8354d1dab2a04
cdflint <cxflint@gmail.com>
2017-09-10 20:40:48 -0400

    :shipit: integrate leaflet draw

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.engine('html', hbs.express4({
 }));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public/img/favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/img/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
