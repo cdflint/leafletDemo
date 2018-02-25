ed3dd1c3c09b7f30b9b805327b6d1d54936c3e9a
cdflint <cxflint@gmail.com>
2018-02-25 17:21:38 -0500

    :shipit: integrate Portal for Arcgis Login option for Secure Services

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Leaflet Demo Fall 2017 CartViz' });
 });
 
+router.get('/callback.html', function(req, res, next) {
+  res.render('callback', { title: 'Login' });
+});
+
 module.exports = router;
